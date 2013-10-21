e2bc67a41a7ae2038f3f69e8b766fbf4081f39af
Cherub Kumar <cherub.kumar@gmail.com>
2013-10-21 00:14:17 -0500

    Finish Layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ SampleApp::Application.routes.draw do
 
 
   get "users/new"
+
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-    root 'static_pages#home'
-
+  root 'static_pages#home'
   # Remaining routes
-
-    match '/help',    to: 'static_pages#help',    via: 'get'
-    match '/about',   to: 'static_pages#about',   via: 'get'
-    match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -51,7 +53,7 @@ SampleApp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
