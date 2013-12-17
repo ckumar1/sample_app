23e52cc83ea19bad83131e4403b39d1baeaa18fd
Cherub Kumar <cherub.kumar@gmail.com>
2013-12-17 00:47:30 -0600

    Finalize basic User model
    Before lab improvements

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  #Convert emails to lowercase before saving in the database
+  before_save { self.email = email.downcase }
+
+  #Name Validations
+  validates :name, presence: true, length: { maximum: 50 }
+
+  #Email Validations
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  #Secure Password
+  has_secure_password
+  #Password Validations
+  validates :password, length: { minimum: 6 }
+
+
+
+
+end
+
+
