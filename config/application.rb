e0e724b3009ca7edeb43490443d298119de5b434
Salini S K <salini.sk4068@gmail.com>
2014-02-03 12:10:50 -0600

    Deployment

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module SampleApp
     # config.i18n.default_locale = :de
 
     # added to make bootstrap compatible with the asset pipeline
+    config.assets.initialize_on_precompile = false
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
   end
