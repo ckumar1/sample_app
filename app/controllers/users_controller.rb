f564eea0f9e77541f6692044241e617fd429f3e0
Agnaldo Luiz <agnaldoluiz@gmail.com>
2014-02-02 21:27:20 -0600

    Layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to MicroMessage!"
       redirect_to @user
     else
       render 'new'
